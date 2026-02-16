a4e85049836770f830c7eee640aa29ddbb45a525
Mohammad Faisal <ayankhan0506@gmail.com>
2026-02-16 11:45:55 +0530

    Delete new directory

--- new/app/chunker.py
+++
@@ -1,8 +0,0 @@
-def chunk_text(text: str, chunk_size: int = 300):
-    words = text.split()
-    chunks = []
-
-    for i in range(0, len(words), chunk_size):
-        chunks.append(" ".join(words[i:i + chunk_size]))
-
-    return chunks
